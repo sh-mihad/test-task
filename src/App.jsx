acb975fd6336f29bd0df1eebacaba17289aa837c
SIFAT-AOC <sifatcreator999@gmail.com>
2024-02-01 22:11:38 +0600

    task app commit first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Hero from "./Hero";
+import TaskBoard from "./TaskBoard.jsx/TaskBoard";
+import '../src/index.css'
+function App() {
+  return <>
+    <Header />
+    <Hero />
+    <TaskBoard />
+    <Footer />
+  </>
+}
+
+export default App;
---
+++ src/TaskData.jsx/TaskData.js
@@ -0,0 +1,20 @@
+export const tasks = [
+    {
+      id: crypto.randomUUID(),
+      title: "Learn React js",
+      description: "I want to learn react and passionate to be a skilled web developer",
+      tags: ["web", "javascript", "react", "nextjs"],
+      priority: "High",
+      isFavourite: true,
+    },
+    {
+      id: crypto.randomUUID(),
+      title: "Learn Vue js",
+      description: "I want to learn Vue for my next project",
+      tags: ["web", "javascript", "vue"],
+      priority: "Medium",
+      isFavourite: false,
+    },
+    // Add 8 more objects here
+  ];
+  
